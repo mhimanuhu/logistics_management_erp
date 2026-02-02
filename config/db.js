b92d3192146a356264fe7194c783084c31692190
MANU SHARMA <71984022+mhimanuhu@users.noreply.github.com>
2026-02-02 07:24:53 +0000

    variable name updated

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "logistics_db"
+  host: process.env.MYSQL_URL || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "root",
+  database: process.env.MYSQL_DATABASE || "logistics_db"
 });
 
 db.connect((err) => {
